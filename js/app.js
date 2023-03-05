682f98ed1a43a52cdd5955ff8a2f233bd51013e0
Ninjax11000 <jamiljoy007@gmail.com>
2023-03-05 22:56:33 +0600

    added features in cards

--- js/app.js
+++ js/app.js
@@ -9,15 +9,18 @@ const Ai= data=>{
     console.log(data);
     const cardContainer=document.getElementById('card-container');
     data.forEach(element => {
-        console.log(element);
+        console.log(element.features);
+       
         const cardDiv=document.createElement('div');
         cardDiv.classList.add('col');
         cardDiv.innerHTML=`
         <div class="card">
                 <img src="${element.image}" class="card-img-top" alt="...">
                 <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <h5 class="card-title">Features</h5>
+                  <ol>
+                  ${element.features.map(a=> `<li> ${a}</li>`)}
+                  </ol>
                 </div>
               </div>
         
